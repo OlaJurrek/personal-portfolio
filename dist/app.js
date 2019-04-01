94cc337a22f93a36fba56e2c12fb1b7b3abe883d
Aleksandra Jurek <jurek.aleksandra@gmail.com>
2019-04-01 18:05:46 +0200

    add auto type heading

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,68 @@
+// Auto type heading
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+// Type Method
+TypeWriter.prototype.type = function() {
+  // Current index of word
+  const current = this.wordIndex % this.words.length;
+  // Get full text of current word
+  const fullTxt = this.words[current];
+  // Check if deleting
+  if (this.isDeleting) {
+    // Remove char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // Add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  // Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  // Initial time speed
+  let typeSpeed = 300;
+
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  // If word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // Make pause at end
+    typeSpeed = this.wait;
+    // Set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    // Move to next word
+    this.wordIndex++;
+    // Pause before start typing
+    typeSpeed = 500;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+// Init on DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
 // Togle menu list
 const menuIcon = document.querySelector(".menu-icon");
 const menuList = document.querySelector(".menu-list");
@@ -13,8 +78,6 @@ function toggleMenu() {
 }
 
 // Close menu list if outside click
-// const nav = document.querySelector("nav");
-
 document.addEventListener("click", clickOutside);
 
 function clickOutside(e) {
@@ -39,5 +102,4 @@ function changeHeader() {
 }
 
 // Initialize Smooth Scroll
-
 const scroll = new SmoothScroll('a[href*="#"]', { speed: 500 });
